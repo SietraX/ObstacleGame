21b4ce1d124d725add01ed05de9ff7073d5a5159
Sietra <canercanturk2004@gmail.com>
2022-07-22 03:16:37 +0300

    Added player model, 3rd person cam, jump functionality

--- Assets/Scripts/ObstacleTriggerer.cs
+++ Assets/Scripts/ObstacleTriggerer.cs
@@ -23,6 +23,5 @@ public class ObstacleTriggerer : MonoBehaviour
         }
         BoxCollider boxCollider = GetComponent<BoxCollider>();
         boxCollider.enabled = false;
-        Debug.Log("test");
     }
 }
